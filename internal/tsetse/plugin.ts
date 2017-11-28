4686d21cc4436fa20c3a14a80b7002e6e74fb45c
Caleb Eggensperger <calebegg@google.com>
2017-11-28 15:31:45 -0500

    First pass at Tsetse plugin

---
+++ internal/tsetse/plugin.ts
@@ -0,0 +1,41 @@
+import * as tsForTypes from 'typescript';
+import * as tsServerLibForTypes from 'typescript/lib/tsserverlibrary';
+
+import * as perfTrace from '../tsc_wrapped/perf_trace';
+import * as pluginApi from '../tsc_wrapped/plugin_api';
+
+import {Checker} from './checker';
+import {registerRules} from './runner';
+
+// Installs the Tsetse language server plugin, which checks Tsetse rules in your
+// editor and shows issues as semantic errors (red squiggly underline).
+
+function init() {
+  return {
+    create(info: ts.server.PluginCreateInfo) {
+      const oldService = info.languageService;
+      const checker = new Checker(oldService.getProgram());
+      // Add disabledRules to tsconfig to disable specific rules
+      // "plugins": [
+      //   {"name": "...", "disabledRules": ["equals-nan"]}
+      // ]
+      //
+      registerRules(checker, info.config.disabledRules || []);
+
+      const proxy = pluginApi.createProxy(oldService);
+      proxy.getSemanticDiagnostics = (fileName: string) => {
+        const result = oldService.getSemanticDiagnostics(fileName);
+        perfTrace.wrap('checkConformance', () => {
+          result.push(
+              ...checker
+                  .execute(oldService.getProgram().getSourceFile(fileName))
+                  .map(failure => failure.toDiagnostic()));
+        });
+        return result;
+      };
+      return proxy;
+    }
+  }
+}
+
+export = init;
