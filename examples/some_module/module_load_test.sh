87c2e24c03e65b51301cdda4d8ee0558e5a3ee60
Greg Magolan <gmagolan@gmail.com>
2018-02-21 12:25:57 -0800

    Cleanup in shell script

--- examples/some_module/module_load_test.sh
+++ examples/some_module/module_load_test.sh
@@ -2,13 +2,13 @@
 set -e
 
 MANIFEST="$TEST_SRCDIR/MANIFEST"
-if [ -e "${MANIFEST}" ]; then
+if [ -e "$MANIFEST" ]; then
   while read line; do
     declare -a PARTS=($line)
     if [ "${PARTS[0]}" == "build_bazel_rules_typescript/examples/some_module/bin" ]; then
       readonly OUT=$(${PARTS[1]})
     fi
-  done < ${MANIFEST}
+  done < $MANIFEST
 else
   readonly OUT=$($TEST_SRCDIR/build_bazel_rules_typescript/examples/some_module/bin)
 fi
