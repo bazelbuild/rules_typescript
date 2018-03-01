eaf8483ff9b6c703cd42cce8c29607fe3d3b47db
Greg Magolan <gmagolan@gmail.com>
2018-03-01 15:48:38 -0800

    Fix comments in internal/tsc_wrapped/path.ts

--- internal/tsc_wrapped/path.ts
+++ internal/tsc_wrapped/path.ts
@@ -1,21 +1,21 @@
 import * as path from 'path';
 
-// resolve function that maintains backslashes
+// resolve function that preserves forward slashes ('/') on Windows
 export function resolve(...a: string[]) {
     return path.resolve(...a).replace(/\\/g, '/');
 }
 
-// join function that maintains backslashes
+// join function that preserves forward slashes ('/') on Windows
 export function join(...a: string[]) {
     return path.join(...a).replace(/\\/g, '/');
 }
 
-// relative function that maintains backslashes
+// relative function that preserves forward slashes ('/') on Windows
 export function relative(a: string, b: string) {
     return path.relative(a, b).replace(/\\/g, '/');
 }
 
-// normalize function that maintains backslashes
+// normalize function that preserves forward slashes ('/') on Windows
 export function normalize(p: string) {
     return path.normalize(p).replace(/\\/g, '/');
 }
