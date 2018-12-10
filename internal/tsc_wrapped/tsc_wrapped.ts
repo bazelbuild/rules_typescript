bazelbuild/rules_typescript

commit 1b33eba25c49d2249945374255579818dc0e94df

Internal only refactoring
PiperOrigin-RevId: 221698301

internal/BUILD.bazel
@@ -107,7 +107,10 @@ jasmine_node_test(
     srcs = [],
     deps = [
         ":test_lib",
+        "@npm//bytebuffer",
         "@npm//jasmine",
+        "@npm//protobufjs",
+        "@npm//source-map",
         "@npm//typescript",
     ],
 )

internal/tsc_wrapped/tsc_wrapped.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import * as path from 'path';
 import * as tsickle from 'tsickle';
 import * as ts from 'typescript';
@@ -7,6 +8,7 @@ import {PLUGIN as bazelConformancePlugin} from '../tsetse/runner';
 import {CachedFileLoader, FileLoader, ProgramAndFileCache, UncachedFileLoader} from './cache';
 import {CompilerHost} from './compiler_host';
 import * as bazelDiagnostics from './diagnostics';
+import {constructManifest} from './manifest';
 import * as perfTrace from './perf_trace';
 import {PLUGIN as strictDepsPlugin} from './strict_deps';
 import {BazelOptions, parseTsconfig, resolveNormalizedPath} from './tsconfig';
@@ -200,48 +202,133 @@ function runFromOptions(
   cache.putProgram(bazelOpts.target, program);
 
 
-  const compilationTargets =
-      program.getSourceFiles().filter(f => isCompilationTarget(bazelOpts, f));
-  const emitResults: ts.EmitResult[] = [];
-  const diagnostics: ts.Diagnostic[] = [];
-  if (bazelOpts.tsickle) {
-    // The 'tsickle' import above is only used in type positions, so it won't
-    // result in a runtime dependency on tsickle.
-    // If the user requests the tsickle emit, then we dynamically require it
-    // here for use at runtime.
-    let optTsickle: typeof tsickle;
-    try {
-      // tslint:disable-next-line:no-require-imports dependency on tsickle only
-      // if requested
-      optTsickle = require('tsickle');
-    } catch {
-      throw new Error(
-          'When setting bazelOpts { tsickle: true }, ' +
-          'you must also add a devDependency on the tsickle npm package');
-    }
-    for (const sf of compilationTargets) {
-      emitResults.push(optTsickle.emitWithTsickle(
-          program, compilerHost, compilerHost, options, sf));
-    }
-    diagnostics.push(
-        ...optTsickle.mergeEmitResults(emitResults as tsickle.EmitResult[])
-            .diagnostics);
-  } else {
-    for (const sf of compilationTargets) {
-      emitResults.push(program.emit(sf));
-    }
+  const compilationTargets = program.getSourceFiles().filter(
+      fileName => isCompilationTarget(bazelOpts, fileName));
 
-    for (const d of emitResults) {
-      diagnostics.push(...d.diagnostics);
-    }
+  let diagnostics: ts.Diagnostic[] = [];
+  let useTsickleEmit = bazelOpts.tsickle;
+  if (useTsickleEmit) {
+    diagnostics = emitWithTsickle(
+        program, compilerHost, compilationTargets, options, bazelOpts);
+  } else {
+    diagnostics = emitWithTypescript(program, compilationTargets);
   }
+
   if (diagnostics.length > 0) {
     console.error(bazelDiagnostics.format(bazelOpts.target, diagnostics));
+    debug('compilation failed at', new Error().stack!);
     return false;
   }
 
   cache.printStats();
   return true;
+}
+
+function emitWithTypescript(
+    program: ts.Program, compilationTargets: ts.SourceFile[]): ts.Diagnostic[] {
+  const diagnostics: ts.Diagnostic[] = [];
+  for (const sf of compilationTargets) {
+    const result = program.emit(sf);
+    diagnostics.push(...result.diagnostics);
+  }
+  return diagnostics;
+}
+
+function emitWithTsickle(
+    program: ts.Program, compilerHost: CompilerHost,
+    compilationTargets: ts.SourceFile[], options: ts.CompilerOptions,
+    bazelOpts: BazelOptions): ts.Diagnostic[] {
+  const emitResults: tsickle.EmitResult[] = [];
+  const diagnostics: ts.Diagnostic[] = [];
+  // The 'tsickle' import above is only used in type positions, so it won't
+  // result in a runtime dependency on tsickle.
+  // If the user requests the tsickle emit, then we dynamically require it
+  // here for use at runtime.
+  let optTsickle: typeof tsickle;
+  try {
+    // tslint:disable-next-line:no-require-imports
+    optTsickle = require('tsickle');
+  } catch (e) {
+    if (e.code !== 'MODULE_NOT_FOUND') {
+      throw e;
+    }
+    throw new Error(
+        'When setting bazelOpts { tsickle: true }, ' +
+        'you must also add a devDependency on the tsickle npm package');
+  }
+  perfTrace.wrap('emit', () => {
+    for (const sf of compilationTargets) {
+      perfTrace.wrap(`emit ${sf.fileName}`, () => {
+        emitResults.push(optTsickle.emitWithTsickle(
+            program, compilerHost, compilerHost, options, sf));
+      });
+    }
+  });
+  const emitResult = optTsickle.mergeEmitResults(emitResults);
+  diagnostics.push(...emitResult.diagnostics);
+
+  // If tsickle reported diagnostics, don't produce externs or manifest outputs.
+  if (diagnostics.length > 0) {
+    return diagnostics;
+  }
+
+  let externs = '/** @externs */\n' +
+      '// generating externs was disabled using generate_externs=False\n';
+  if (bazelOpts.tsickleGenerateExterns) {
+    externs =
+        optTsickle.getGeneratedExterns(emitResult.externs, options.rootDir!);
+  }
+
+  if (bazelOpts.tsickleExternsPath) {
+    // Note: when tsickleExternsPath is provided, we always write a file as a
+    // marker that compilation succeeded, even if it's empty (just containing an
+    // @externs).
+    fs.writeFileSync(bazelOpts.tsickleExternsPath, externs);
+
+    // When generating externs, generate an externs file for each of the input
+    // .d.ts files.
+    if (bazelOpts.tsickleGenerateExterns &&
+        compilerHost.provideExternalModuleDtsNamespace) {
+      for (const extern of compilationTargets) {
+        if (!extern.isDeclarationFile) continue;
+        const outputBaseDir = options.outDir!;
+        const relativeOutputPath =
+            compilerHost.relativeOutputPath(extern.fileName);
+        mkdirp(outputBaseDir, path.dirname(relativeOutputPath));
+        const outputPath = path.join(outputBaseDir, relativeOutputPath);
+        const moduleName = compilerHost.pathToModuleName('', extern.fileName);
+        fs.writeFileSync(
+            outputPath,
+            `goog.module('${moduleName}');\n` +
+                `// Export an empty object of unknown type to allow imports.\n` +
+                `// TODO: use typeof once available\n` +
+                `exports = /** @type {?} */ ({});\n`);
+      }
+    }
+  }
+
+  if (bazelOpts.manifest) {
+    perfTrace.wrap('manifest', () => {
+      const manifest =
+          constructManifest(emitResult.modulesManifest, compilerHost);
+      fs.writeFileSync(bazelOpts.manifest, manifest);
+    });
+  }
+
+  return diagnostics;
+}
+
+/**
+ * Creates directories subdir (a slash separated relative path) starting from
+ * base.
+ */
+function mkdirp(base: string, subdir: string) {
+  const steps = subdir.split(path.sep);
+  let current = base;
+  for (let i = 0; i < steps.length; i++) {
+    current = path.join(current, steps[i]);
+    if (!fs.existsSync(current)) fs.mkdirSync(current);
+  }
 }
 
 

examples/googmodule/a.ts
@@ -1,1 +1,1 @@
-export const a: number = 1;
+const a: number = 1;

internal/tsc_wrapped/tsc_wrapped_test.ts
@@ -1,0 +1,287 @@
+import * as path from 'path';
+import {ModulesManifest} from 'tsickle';
+import * as ts from 'typescript';
+
+import {UncachedFileLoader} from './cache';
+import {CompilerHost} from './compiler_host';
+import {constructManifest} from './manifest';
+import {writeTempFile} from './test_support';
+import {BazelOptions} from './tsconfig';
+
+// tslint:disable-next-line:no-any mock for testing.
+const throwingCompilerHostFake: ts.CompilerHost = null as any;
+
+const testFileLoader = new UncachedFileLoader();
+
+const relativeOutputPath = (f: string) => f;
+
+type ModuleResolver =
+    (moduleName: string, containingFile: string,
+     compilerOptions: ts.CompilerOptions, host: ts.ModuleResolutionHost) =>
+        ts.ResolvedModuleWithFailedLookupLocations;
+
+describe('tsc wrapped', () => {
+  function f(
+      res: ModulesManifest, fname: string, module: string, deps: string[]) {
+    res.addModule(fname, module);
+    for (let i = 0; i < deps.length; i++) {
+      res.addReferencedModule(fname, deps[i]);
+    }
+    return res;
+  }
+
+  it('produces a topo-sorted manifest', () => {
+    const res = new ModulesManifest();
+    f(res, 'src/f3.js', 'test$f3', ['test$f2', 'test$f1']);
+    f(res, 'src/f2.js', 'test$f2',
+      ['external$ts_source_not_included', 'test$f1']);
+    f(res, 'src/f1.js', 'test$f1', []);
+    expect(constructManifest(res, {relativeOutputPath})).toBe([
+      'src/f1.js\n', 'src/f2.js\n', 'src/f3.js\n'
+    ].join(''));
+  });
+
+  it('reports cyclical dependencies', () => {
+    const res = new ModulesManifest();
+    f(res, 'src/f2', 'src$f2', ['src$f3']);
+    f(res, 'src/f3', 'src$f3', ['src$f1']);
+    f(res, 'src/f1', 'src$f1', ['src$f2']);
+    expect(() => constructManifest(res, {relativeOutputPath}))
+        .toThrowError(/src\/f2 -> src\/f3 -> src\/f1 -> src\/f2/g);
+  });
+
+  it('toposorts diamonds', () => {
+    //   t
+    // l   r
+    //   b
+    const res = new ModulesManifest();
+    f(res, 'bottom.js', 'bottom', ['left', 'right']);
+    f(res, 'right.js', 'right', ['top']);
+    f(res, 'left.js', 'left', ['top']);
+    f(res, 'top.js', 'top', []);
+    expect(constructManifest(res, {relativeOutputPath})).toBe([
+      'top.js\n',
+      'left.js\n',
+      'right.js\n',
+      'bottom.js\n',
+    ].join(''));
+  });
+
+});
+
+// Create something that looks like CompilerOptions.
+const COMPILER_OPTIONS: ts.CompilerOptions = {
+  rootDirs: [
+    // Sorted by inverse length, as done by tsconfig.ts in production.
+    '/root/google3/blaze-out/k8-fastbuild/genfiles',
+    '/root/google3/blaze-out/k8-fastbuild/bin',
+    '/root/google3',
+  ],
+  outDir: '/root/google3/blaze-out/k8-fastbuild/bin',
+  rootDir: '/root/google3'
+};
+
+
+const defaultBazelOpts = {
+  googmodule: true,
+  workspaceName: 'google3',
+  prelude:
+      `goog.require('google3.third_party.javascript.tslib.tslib_closure');`,
+} as BazelOptions;
+
+describe('compiler host', () => {
+  const bazelOpts = {
+    ...defaultBazelOpts,
+    es5Mode: false,
+  } as BazelOptions;
+
+  it('looks up files', () => {
+    const fn = writeTempFile('file_lookup', 'let x: number = 123;');
+    const fn2 = writeTempFile('file_lookup2', 'let x: number = 124;');
+    const host = new CompilerHost(
+        [fn /* but not fn2! */], COMPILER_OPTIONS, bazelOpts,
+        throwingCompilerHostFake, testFileLoader);
+    expect(host.fileExists(fn)).toBe(true);
+    expect(host.fileExists(fn2)).toBe(false);
+  });
+
+  describe('file writing', () => {
+    let writtenFiles: {[key: string]: string};
+    beforeEach(() => writtenFiles = {});
+
+    const delegateHost = {
+      writeFile: (fn: string, d: string) => {
+        writtenFiles[fn.replace(/\\/g, '/')] = d;
+      }
+      // tslint:disable-next-line:no-any mock for testing.
+    } as any;
+
+    function createFakeModuleResolver(
+        moduleRoots: {[moduleName: string]: string}): ModuleResolver {
+      return (moduleName: string, containingFile: string,
+              compilerOptions: ts.CompilerOptions,
+              host: ts.ModuleResolutionHost) => {
+        if (moduleName[0] === '.') {
+          moduleName =
+              path.posix.join(path.dirname(containingFile), moduleName);
+        }
+        for (const moduleRoot in moduleRoots) {
+          if (moduleName.indexOf(moduleRoot) === 0) {
+            const resolvedFileName = moduleRoots[moduleRoot] +
+                moduleName.substring(moduleRoot.length) + '.d.ts';
+            return {
+              resolvedModule: {resolvedFileName, extension: ts.Extension.Dts},
+              failedLookupLocations: []
+            };
+          }
+        }
+
+        return {
+          resolvedModule:
+              {resolvedFileName: moduleName, extension: ts.Extension.Dts},
+          failedLookupLocations: []
+        };
+      };
+    }
+
+    function createFakeGoogle3Host({
+      es5 = false,
+      moduleRoots = {} as {[moduleName: string]: string},
+      isJsTranspilation = false,
+      transpiledJsOutputFileName = undefined as string | undefined,
+    } = {}) {
+      const bazelOpts = {
+        ...defaultBazelOpts,
+        es5Mode: es5,
+        isJsTranspilation,
+        transpiledJsOutputFileName,
+      } as BazelOptions;
+      return new CompilerHost(
+          [], COMPILER_OPTIONS, bazelOpts, delegateHost, testFileLoader,
+          createFakeModuleResolver(moduleRoots));
+    }
+
+    describe('converts path to module names', () => {
+      let host: CompilerHost;
+      beforeEach(() => {
+        host = createFakeGoogle3Host({
+          moduleRoots: {
+            'module': 'path/to/module',
+            'module2': 'path/to/module2',
+            'path/to/module2': 'path/to/module2',
+          },
+        });
+      });
+
+      function expectPath(context: string, path: string) {
+        return expect(host.pathToModuleName(context, path));
+      }
+
+      it('mangles absolute paths', () => {
+        expectPath('whatever/context', 'some/absolute/module')
+            .toBe('google3.some.absolute.module');
+      });
+
+      it('escapes special symbols', () => {
+        expectPath('', 'some|123').toBe('google3.some$7c123');
+        expectPath('', '1some|').toBe('google3.1some$7c');
+        expectPath('', 'bar/foo.bam.ts').toBe('google3.bar.foo$2ebam');
+        // Underscore is unmodified, because it is common in google3 paths.
+        expectPath('', 'foo_bar').toBe('google3.foo_bar');
+      });
+
+      it('resolves paths', () => {
+        const context = 'path/to/module';
+
+        expectPath(context, './module2').toBe('google3.path.to.module2');
+        expectPath(context, '././module2').toBe('google3.path.to.module2');
+        expectPath(context, '../to/module2').toBe('google3.path.to.module2');
+        expectPath(context, '../to/.././to/module2')
+            .toBe('google3.path.to.module2');
+      });
+
+      it('ignores extra google3 sections in paths', () => {
+        expectPath('', 'google3/foo/bar').toBe('google3.foo.bar');
+      });
+
+      it('resolves absolute paths', () => {
+        const context = 'path/to/module/dir/file';
+
+        expectPath(context, '/root/google3/some/file.ts')
+            .toBe('google3.some.file');
+        expectPath(context, '/root/google3/path/to/some/file')
+            .toBe('google3.path.to.some.file');
+        expectPath(
+            context, '/root/google3/blaze-out/k8-fastbuild/bin/some/file')
+            .toBe('google3.some.file');
+        expectPath(
+            context, '/root/google3/blaze-out/k8-fastbuild/genfiles/some/file')
+            .toBe('google3.some.file');
+      });
+
+      describe('uses module name for resolved file paths', () => {
+        it('for goog.module module names', () => {
+          expectPath('', 'module/dir/file2')
+              .toBe('google3.path.to.module.dir.file2');
+          expectPath('', 'module2/dir/file2')
+              .toBe('google3.path.to.module2.dir.file2');
+        });
+
+        it('for imports of files from the same module', () => {
+          const context = 'path/to/module/dir/file';
+
+          expectPath(context, 'module/dir/file2')
+              .toBe('google3.path.to.module.dir.file2');
+          expectPath(context, './foo/bar')
+              .toBe('google3.path.to.module.dir.foo.bar');
+          expectPath(context, '../foo/bar')
+              .toBe('google3.path.to.module.foo.bar');
+          expectPath(context, 'path/to/module/dir/file2')
+              .toBe('google3.path.to.module.dir.file2');
+        });
+
+        it('for imports of files from a different module', () => {
+          const context = 'path/to/module/dir/file';
+
+          expectPath(context, 'module2/dir/file')
+              .toBe('google3.path.to.module2.dir.file');
+          expectPath(context, '../../module2/dir/file')
+              .toBe('google3.path.to.module2.dir.file');
+          expectPath(context, 'path/to/module2/dir/file')
+              .toBe('google3.path.to.module2.dir.file');
+        });
+      });
+    });
+
+    describe('output files', () => {
+      it('writes to .closure.js in ES6 mode', () => {
+        createFakeGoogle3Host({
+          es5: false,
+        }).writeFile('a.js', 'some.code();', false, undefined, []);
+        expect(Object.keys(writtenFiles)).toEqual([
+          '/root/google3/blaze-out/k8-fastbuild/bin/a.closure.js'
+        ]);
+      });
+
+      it('writes to .js in ES5 mode', () => {
+        createFakeGoogle3Host({
+          es5: true,
+        }).writeFile('a/b.js', 'some.code();', false, undefined, []);
+        expect(Object.keys(writtenFiles)).toEqual([
+          '/root/google3/blaze-out/k8-fastbuild/bin/a/b.js'
+        ]);
+      });
+
+      it('writes to closureOptions.transpiledJsOutputFileName in JS transpilation mode',
+         () => {
+           createFakeGoogle3Host({
+             isJsTranspilation: true,
+             transpiledJsOutputFileName: 'foo/bar/a/b.dev_es5.js',
+           }).writeFile('a/b.js', 'some.code();', false, undefined, []);
+           expect(Object.keys(writtenFiles)).toEqual([
+             '/root/google3/blaze-out/k8-fastbuild/bin/foo/bar/a/b.dev_es5.js'
+           ]);
+         });
+    });
+  });
+});

commit 4bb7a3c8e408fc924295042cbe0df1fe59f5b100

Call gatherDiagnostics to perform syntax/semantic/type checks
This was accidentally removed in recent refactoring https://github.com/bazelbuild/rules_typescript/commit/1b33eba25c49d2249945374255579818dc0e94df
which caused TS compilations to incorrectly succeed when the code contains errors.

Restore missing tests which would have caught the regression; these were also accidentally disabled when Bazel BUILD files were refactored.

PiperOrigin-RevId: 224869541

internal/BUILD.bazel
@@ -17,11 +17,11 @@
 package(default_visibility = ["//visibility:public"])
 
 exports_files([
-    "tsconfig.json",
     # Exported to be consumed for generating skydoc.
     "build_defs.bzl",
     "ts_config.bzl",
     "ts_repositories.bzl",
+    "tsetse/tsconfig.json",
 ])
 
 load("//internal:defaults.bzl", "ts_library")
@@ -98,6 +98,7 @@ ts_library(
         ":tsc_wrapped",
         "@npm//@types/jasmine",
         "@npm//@types/node",
+        "@npm//tsickle",
         "@npm//typescript",
     ],
 )

internal/tsc_wrapped/tsc_wrapped.ts
@@ -205,6 +205,30 @@ function runFromOptions(
           compilerHost.inputFiles, options, compilerHost, oldProgram));
   cache.putProgram(bazelOpts.target, program);
 
+  if (!bazelOpts.isJsTranspilation) {
+    // If there are any TypeScript type errors abort now, so the error
+    // messages refer to the original source.  After any subsequent passes
+    // (decorator downleveling or tsickle) we do not type check.
+    let diagnostics =
+        gatherDiagnostics(options, bazelOpts, program, disabledTsetseRules);
+    if (!expectDiagnosticsWhitelist.length ||
+        expectDiagnosticsWhitelist.some(p => bazelOpts.target.startsWith(p))) {
+      diagnostics = bazelDiagnostics.filterExpected(
+          bazelOpts, diagnostics, bazelDiagnostics.uglyFormat);
+    } else if (bazelOpts.expectedDiagnostics.length > 0) {
+      console.error(
+          `Only targets under ${
+              expectDiagnosticsWhitelist.join(', ')} can use ` +
+              'expected_diagnostics, but got',
+          bazelOpts.target);
+    }
+
+    if (diagnostics.length > 0) {
+      console.error(bazelDiagnostics.format(bazelOpts.target, diagnostics));
+      debug('compilation failed at', new Error().stack!);
+      return false;
+    }
+  }
 
   const compilationTargets = program.getSourceFiles().filter(
       fileName => isCompilationTarget(bazelOpts, fileName));

internal/tsetse/tests/ban_expect_truthy_promise/BUILD
@@ -29,7 +29,7 @@ ts_library(
 ts_library(
     name = "positives",
     testonly = 1,
-    tsconfig = "//internal/tsetse:tsconfig.json",
+    tsconfig = "//internal:tsetse/tsconfig.json",
     srcs = [
         "positives.ts",
     ],
@@ -50,7 +50,7 @@ ts_library(
 ts_library(
     name = "negatives",
     testonly = 1,
-    tsconfig = "//internal/tsetse:tsconfig.json",
+    tsconfig = "//internal:tsetse/tsconfig.json",
     srcs = [
         "negatives.ts",
     ],

internal/tsetse/tests/ban_promise_as_condition/BUILD
@@ -19,14 +19,9 @@ load("//internal:defaults.bzl", "ts_library")
 error_message = "error TS21226: Found a thenable.*"
 
 ts_library(
-    name = "jasmine",
-    testonly = 1,
-)
-
-ts_library(
     name = "positives",
     testonly = 1,
-    tsconfig = "//internal/tsetse:tsconfig.json",
+    tsconfig = "//internal:tsetse/tsconfig.json",
     srcs = [
         "positives.ts",
     ],
@@ -49,7 +44,7 @@ ts_library(
 ts_library(
     name = "negatives",
     testonly = 1,
-    tsconfig = "//internal/tsetse:tsconfig.json",
+    tsconfig = "//internal:tsetse/tsconfig.json",
     srcs = ["negatives.ts"],
     deps = [
     ],

internal/tsetse/tests/check_return_value/BUILD
@@ -23,7 +23,7 @@ ts_library(
     name = "no_expected_diagnostics_test",
     testonly = 1,
     srcs = ["no_expected_diagnostics.ts"],
-    tsconfig = "//third_party/bazel_rules/rules_typescript/internal/tsetse:tsconfig.json",
+    tsconfig = "//internal:tsetse/tsconfig.json",
 )
 
 ts_library(
@@ -37,14 +37,14 @@ ts_library(
         "\(16,1\).*" + error_message,
         "\(19,1\).*" + error_message,
     ],
-    tsconfig = "//third_party/bazel_rules/rules_typescript/internal/tsetse:tsconfig.json",
+    tsconfig = "//internal:tsetse/tsconfig.json",
 )
 
 ts_library(
     name = "user_defined_check_return_value",
     testonly = 1,
     srcs = ["user_defined_check_return_value.ts"],
-    tsconfig = "//third_party/bazel_rules/rules_typescript/internal/tsetse:tsconfig.json",
+    tsconfig = "//internal:tsetse/tsconfig.json",
 )
 
 ts_library(
@@ -57,9 +57,8 @@ ts_library(
         "\(7,1\).*" + error_message,
         "\(9,1\).*" + error_message,
         "\(15,1\).*" + error_message,
-        "\(16,1\).*" + error_message,
     ],
-    tsconfig = "//third_party/bazel_rules/rules_typescript/internal/tsetse:tsconfig.json",
+    tsconfig = "//internal:tsetse/tsconfig.json",
     deps = [
         ":user_defined_check_return_value",
         "@npm//@types/jasmine",

internal/tsetse/tests/must_use_promises/BUILD
@@ -25,7 +25,7 @@ ts_library(
     srcs = [
         "positives.ts",
     ],
-    tsconfig = "//third_party/bazel_rules/rules_typescript/internal/tsetse:tsconfig.json",
+    tsconfig = "//internal:tsetse/tsconfig.json",
     expected_diagnostics = [
         "\(29,3\)" + error_message,
         "\(30,3\)" + error_message,
@@ -33,5 +33,6 @@ ts_library(
         "\(32,3\)" + error_message,
         "\(34,3\)" + error_message,
     ],
-    deps = ["//third_party/javascript/node_modules/typescript:es2015.promise"],
+    deps = [
+    ],
 )