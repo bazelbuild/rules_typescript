1bf1739313adad1c82331c2ed079545bc646c57c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-01-17 20:01:48 +0100

    Add logic to resolve runfiles within G3 without using external runfile helpers

--- devserver/runfiles/runfiles.go
+++ devserver/runfiles/runfiles.go
@@ -1,54 +1,28 @@
-// Copyright 2018 The Bazel Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+// Runfiles package that provides utility helpers for resolving Bazel runfiles within Go. This is an abstraction
+// needed for G3 because we don't want to sync the "rules_go" runfile helpers yet.
 package runfiles
 
 import (
-	"errors"
+	"fmt"
 	"os"
+	"path/filepath"
+
+	"github.com/bazelbuild/rules_go/go/tools/bazel"
 )
 
-const (
-	runfilesManifestFile = "RUNFILES_MANIFEST_FILE"
-	runfilesDir          = "RUNFILES_DIR"
-)
+var isG3 = false
 
-var errNoRunfilesEnv = errors.New("runfiles environment missing")
+func Runfile(manifestPath string) (string, error) {
+	if isG3 {
+		runfilesDir := os.Getenv("RUNFILES")
 
-// runfilesResolver is an interface for a resolver that can take a runfiles path and resolve it to a path on
-// disk.
-type runfilesResolver interface {
-	Resolve(string) (string, bool)
-}
-
-// newRunfilesResolver creates a new runfiles resolver. The type of resolver and its parameters are derived
-// from the environment.
-func newRunfilesResolver() (runfilesResolver, error) {
-	manifest := os.Getenv(runfilesManifestFile)
-	if manifest != "" {
-		f, err := os.Open(manifest)
-		if err != nil {
-			return nil, err
+		if len(runfilesDir) == 0 {
+			return "", fmt.Errorf("could not find bazel runfiles directory. \"RUNFILES\" environment " +
+				"variable is not set")
 		}
-		defer f.Close()
-		return newManifestRunfilesResolver(f)
+
+		return filepath.Join(runfilesDir, manifestPath), nil
 	}
 
-	directory := os.Getenv(runfilesDir)
-	if directory != "" {
-		return newDirectoryRunfilesResolver(directory)
-	}
-
-	return nil, errNoRunfilesEnv
+	return bazel.Runfile(manifestPath)
 }
